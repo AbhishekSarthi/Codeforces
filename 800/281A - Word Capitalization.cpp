238574eca4e086dd45da83ac239565546aa0f9d0
Abhishek Sarthi <59399188+AbhishekSarthi@users.noreply.github.com>
2021-01-03 19:18:44 +0530

    Add files via upload

---
+++ 800/281A - Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s;
+	cin>>s;
+	int n = s.length();
+	for(int i=0;i<n;i++){
+        if(i==0)
+	        putchar(toupper(s[i]));
+        else
+            putchar(s[i]);
+
+	}
+	return 0;
+}
